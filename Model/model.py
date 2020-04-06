b6298ccb4c5f22e6013ea25fdba9d3af203e3f13
eoinmca <eoinmc11@gmail.com>
2020-04-06 16:12:06 +0100

    Initial commit

---
+++ Model/model.py
@@ -0,0 +1,132 @@
+import os
+import gym
+import random
+import datetime
+import numpy as np
+import tensorflow as tf
+
+from gym import wrappers
+from _collections import deque
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model, Sequential, Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, Input, InputLayer
+
+
+class DQN:
+    def __init__(self, dqn_env, num_actions, batch_size):
+        self.env = dqn_env
+        self.output_dim = num_actions
+        self.replay_memory = deque(maxlen=2000)
+
+        self.gamma = 0.9
+        self.batch_size = batch_size
+        self.tau = 0.125
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.agent_name = 'DQN'
+        self.learning_rate = 0.005
+        self.model_directory = 'Models'
+        self.target_update_counter = 0
+
+        if not os.path.exists(self.model_directory):
+            os.mkdir(self.model_directory)
+
+        if os.path.exists(self.model_directory + '/' + self.agent_name):
+            print('Model found: Loading.....', '\n')
+            self.model = load_model(self.model_directory + '/' + self.agent_name)
+        else:
+            print('No Model Found: Creating....')
+            self.model = self.create_model()
+            self.save_model(self.model_directory + '/' + self.agent_name)
+
+        # Create Target Model
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+    def create_model(self):
+        model = Sequential()
+        state_shape = (96, 96, 3)
+        model.add(Conv2D(input_shape=state_shape,
+                         data_format='channels_last',
+                         filters=256,
+                         kernel_size=(3, 3),
+                         strides=(2, 2),
+                         activation='relu'))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.2))
+        model.add(Flatten())
+        model.add(Dense(units=64, kernel_initializer='zeros', activation='relu'))
+        model.add(Dense(units=self.output_dim, kernel_initializer='zeros', activation='softmax'))
+        model.compile(loss="mean_squared_error", optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def save_model(self, fn):
+        self.model.save(fn, save_format='tf')
+
+    def update_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def target_train(self):
+        # weights = self.model.get_weights()
+        # target_weights = self.target_model.get_weights()
+        # for i in range(len(target_weights)):
+        #     target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        # self.target_model.set_weights(target_weights)
+        self.target_model.set_weights(self.model.get_weights())
+
+    def get_action(self, state):
+        if state.ndim is 3:
+            state = np.expand_dims(state, axis=0)
+        self.epsilon *= self.epsilon_decay
+        # noinspection PyAttributeOutsideInit
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        if np.random.random() < self.epsilon:
+            action = random.randint(0, self.output_dim - 1)
+            # print('Random', action)
+            return action
+        action = np.argmax(self.model.predict(state)[0])
+        print('Predicted', action, self.model.predict(state))
+        return action
+
+    def train_model(self, terminal_state):
+        if len(self.replay_memory) < self.batch_size:
+            return
+
+        samples = random.sample(self.replay_memory, self.batch_size)
+
+        current_states = np.array([transition[0] for transition in samples]) / 255
+        current_qs_list = self.model.predict(current_states)
+
+        new_current_states = np.array([transition[3] for transition in samples]) / 255
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(samples):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + self.gamma * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        self.model.fit(np.array(X) / 255, np.array(y), batch_size=self.batch_size, verbose=0, shuffle=False)
+        self.model.save(self.model_directory + '/' + self.agent_name)
+
+        if terminal_state:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > 5:
+            print('Target Train')
+            self.target_train()
+            self.target_update_counter = 0
+
+
+
---
+++ main.py
@@ -0,0 +1,86 @@
+import numpy as np
+import gym
+import tensorflow as tf
+import datetime
+from Model import model
+from Racer import Racer
+from _collections import deque
+
+if __name__ == "__main__":
+
+    reward_memory_length = 20
+    reward_memory = deque(maxlen=reward_memory_length)
+
+    def epsilon_greedy(etiles, ereward):
+        reward_memory.append(ereward)
+        if len(reward_memory) < reward_memory_length:
+            print(0.8)
+            return 0.8
+
+
+        min_reward = -0.1 * reward_memory_length
+        min_reward -= min_reward
+        max_reward = (1000 / etiles) * reward_memory_length + min_reward
+        max_reward = int(max_reward)
+        reward_sum = sum(reward_memory)
+        epsilon_spread = 1 / max_reward
+        epsilon = 1 - reward_sum * epsilon_spread
+        print(epsilon)
+        return epsilon
+
+    actions = (np.array([-1.0, 0.0, 0.0]),
+               np.array([-0.5, 0.0, 0.0]),
+               np.array([+1.0, 0.0, 0.0]),
+               np.array([+0.5, 0.0, 0.0]),
+               np.array([0.0, +0.5, 0.0]),
+               np.array([0.0, +1.0, 0.0]),
+               np.array([0.0, +0.5, 0.0]),
+               np.array([0.0, 0.0, +0.8]),
+               np.array([0.0, 0.0, +0.3]))
+
+    num_actions = len(actions)
+    env = Racer.CarRacing()
+    # env = gym.make('CarRacing-v0')
+    env.render()
+    current_state = env.reset()
+    agent = model.DQN(env, num_actions, 32)
+    record_video = False
+    isopen = True
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = 'logs/dqn/' + current_time
+    summary_writer = tf.summary.create_file_writer(log_dir)
+
+    reward_kill = -20.0
+    episode = 1
+    while isopen:
+        env.reset()
+        tiles = env.get_tiles()
+        print(tiles)
+        print('Episode', episode, 'Starting')
+        total_reward = 0.0
+        steps = 0
+        restart = False
+        training = True
+        reward = 0
+
+        while True:
+            # epsilon = epsilon_greedy(tiles, reward)
+            act = agent.get_action(current_state)
+            action = actions[act]
+            new_state, reward, done, info = env.step(action)
+            agent.update_memory([current_state, act, reward, new_state, done])
+            if training:
+                agent.train_model(done)
+            total_reward += reward
+            steps += 1
+            isopen = env.render()
+            current_state = new_state
+            if steps % 200 is 0:
+                print('Episode Number:', episode, ' | Tot Reward:', total_reward, ' | Steps:', steps)
+            if done or restart or isopen is False or total_reward < reward_kill:
+                if episode % 5 == 5:
+                    agent.target_train()
+                agent.save_model('Models/DQN')
+                episode += 1
+                break
+    env.close()
